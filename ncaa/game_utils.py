710554ed97dd8be9c8f80794cf2006975171f9f8
Derek Willis <dwillis@gmail.com>
2021-04-02 21:28:36 -0400

    updated teams with urls and pbp parser

--- ncaa/game_utils.py
+++ ncaa/game_utils.py
@@ -1,34 +1,29 @@
 import os
 import re
 import json
+from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
 
-base_url = "https://umterps.com/sports/womens-basketball/stats/"
-
-def fetch_season(season):
-    game_ids = fetch_game_ids(season)
-    domain = parse_domain(base_url)
+def fetch_season(season, base_url):
+    stats_url = base_url+"/stats/"
+    game_ids = fetch_game_ids(season, stats_url)
+    domain = parse_domain(stats_url)
     parse_games(season, domain, game_ids)
 
-def build_url(season, section):
-    return base_url + season + "#" + section
+def build_url(stats_url, season, section):
+    return stats_url + season + "#" + section
 
 def fetch_url(url):
     r = requests.get(url)
     return r
 
-def fetch_game_ids(season):
-    url = build_url(season, 'game')
+def fetch_game_ids(season, stats_url):
+    url = build_url(stats_url, season, 'game')
     r = fetch_url(url)
-    if season == '2019-20':
-        season_js = re.search(r"var obj = (.*);", r.text).group(1)
-        season_json = json.loads(season_js)
-        game_ids = [x['id'] for x in season_json['data']]
-    else:
-        season_html = BeautifulSoup(r.text, features="html.parser")
-        games = season_html.find('section', {'id': 'game-team'}).findAll('a')
-        game_ids = [x['href'].split("id=")[1].replace("&path=wbball","") for x in games]
+    season_html = BeautifulSoup(r.text, features="html.parser")
+    games = season_html.find('section', {'id': 'game-team'}).findAll('a')
+    game_ids = [x['href'].split("id=")[1].replace("&path=wbball","") for x in games]
     return game_ids
 
 def parse_games(season, domain, game_ids):
@@ -37,11 +32,19 @@ def parse_games(season, domain, game_ids):
         game_json = fetch_game_json(domain, game_id)
         write_json(game_id, game_json, season)
 
+def parse_domain(url):
+    domain = urlparse(url).netloc
+    return domain
+
 def fetch_game_json(domain, game_id):
     url = f"https://{domain}/api/livestats?game_id={game_id}&detail=full"
+    print(url)
     r = fetch_url(url)
     if r.status_code == 200:
-        pbp = r.json()
+        try:
+            pbp = r.json()
+        except:
+            pbp = None
     else:
         pbp = None
     return pbp
