dd2cc2ca360559f6e00fa6cbf631af774d15a669
Derek Willis <dwillis@gmail.com>
2021-09-05 13:07:35 -0400

    rescraped FIBA games

--- fiba/scrape_boxscore.py
+++ fiba/scrape_boxscore.py
@@ -1,11 +1,14 @@
 import time
 import requests
+import requests_cache
 import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime
 from sqlite_utils import Database
 
 BASE_URL = "https://www.fiba.basketball"
+
+requests_cache.install_cache('fiba_cache')
 
 def get_game_urls(event_slug):
     page = requests.get(BASE_URL + event_slug).text
@@ -16,16 +19,15 @@ def get_game_urls(event_slug):
 
 def get_players(link_ls):
     all_tables = []
-
     for a in link_ls:
-        if a == 'https://www.fiba.basketball/u16americas/women/2021/game/2908/Canada-USA':
-            a = '/u16americas/women/2021/game/2908/Canada-USA'
+        if a == None:
+            continue
         try:
             all_tables.append(get_boxscore(BASE_URL + a))
         except (ValueError, TypeError, AttributeError):
             print("Problem with", BASE_URL + a)
             raise
-        time.sleep(3)
+        time.sleep(1)
     return all_tables
 
 def get_boxscore(game_url):
@@ -49,90 +51,11 @@ def get_boxscore(game_url):
     scores = [s for s in box_sc.find("div", class_= "final-score").text.split("\n") if s]
     date = game_url.split("/")[-2]
 
-
     loc_box, aw_box = boxsc.find_all("tbody")
     colnames = [d.text for d in boxsc.find("thead").find_all("th")]
-    names = game_url.split("/")[-1].split("-")
+    teams = [s.text for s in box_sc.find_all('span', class_='team-name')[:2]]
 
     all_players = []
-
-    for i in range(len(names)):
-        if "Republic" in game_url:
-            if names[i].startswith("Republic"):
-                names[i] = "Dominican Republic"
-            elif names[i].startswith("Dominican"):
-                names[i] = "Dominican Republic"
-        if "Bosnia" in game_url:
-            if names[i].startswith("Bosnia"):
-                names[i] = "Bosnia & Herzegovina"
-            elif names[i].startswith("Herzegovina"):
-                names[i] = "Bosnia & Herzegovina"
-            elif names[i].startswith("and"):
-                names[i] = "Bosnia & Herzegovina"
-        if "Salvador" in game_url:
-            if names[i].startswith("Salvador"):
-                names[i] = "El Salvador"
-            elif names[i].startswith("El"):
-                names[i] = "El Salvador"
-        if "Rico" in game_url:
-            if names[i].startswith("Rico"):
-                names[i] = "Puerto Rico"
-            elif names[i].startswith("Puerto"):
-                names[i] = "Puerto Rico"
-        if "Virgin" in game_url:
-            if names[i].startswith("Islands"):
-                names[i] = "Virgin Islands"
-            elif names[i].startswith("Virgin"):
-                names[i] = "Virgin Islands"
-            elif names[i].startswith("US"):
-                names[i] = "Virgin Islands"
-        if "Taipei" in game_url:
-            if names[i].startswith("Taipei"):
-                names[i] = "Chinese Taipei"
-            elif names[i].startswith("Chinese"):
-                names[i] = "Chinese Taipei"
-        if "Zealand" in game_url:
-            if names[i].startswith("Zealand"):
-                names[i] = "New Zealand"
-            elif names[i].startswith("New"):
-                names[i] = "New Zealand"
-        if "Britain" in game_url:
-            if names[i].startswith("Great"):
-                names[i] = "Great Britain"
-            elif names[i].startswith("Britain"):
-                names[i] = "Great Britain"
-        if "Macedonia" in game_url:
-            if names[i].startswith("North"):
-                names[i] = "North Macedonia"
-            elif names[i].startswith("Macedonia"):
-                names[i] = "North Macedonia"
-        if "Cook" in game_url:
-            if names[i].startswith("Cook"):
-                names[i] = "Cook Islands"
-            elif names[i].startswith("Islands"):
-                names[i] = "Cook Islands"
-        if "Caledonia" in game_url:
-            if names[i].startswith("New"):
-                names[i] = "New Caledonia"
-            elif names[i].startswith("Caledonia"):
-                names[i] = "New Caledonia"
-        if "Caledonia" in game_url:
-            if names[i].startswith("New"):
-                names[i] = "New Caledonia"
-            elif names[i].startswith("Caledonia"):
-                names[i] = "New Caledonia"
-        if "Marshall" in game_url:
-            if names[i].startswith("Marshall"):
-                names[i] = "Marshall Islands"
-            elif names[i].startswith("Islands"):
-                names[i] = "Marshall Islands"
-        if "Papua" in game_url:
-            if names[i].startswith("Papua"):
-                names[i] = "Papua New Guinea"
-            elif names[i].startswith("New"):
-                names[i] = "Papua New Guinea"
-            elif names[i].startswith("Guinea"):
-                names[i] = "Papua New Guinea"
 
     for i, team in enumerate([loc_box, aw_box]):
         for player in team.find_all("tr"):
@@ -140,7 +63,7 @@ def get_boxscore(game_url):
             if len(example) < len(colnames):
                 example = example[:-1]
                 example = example + ["0:0", "0"] + ["0/0"] * 4 + ["0"] * 10
-            player_data = {"country": names[i], "vs": names[i-1],
+            player_data = {"country": teams[i], "vs": teams[i-1],
                            "team_score": scores[i], "vs_score": scores[i-1],
                            "date": date}
             for a,b in zip(colnames, example):
@@ -151,8 +74,8 @@ def get_boxscore(game_url):
     return pd.DataFrame(all_players)
 
 if __name__ == "__main__":
-    event_slug = "/en/u16americas/women/2021/games"
+    event_slug = "/oceania/u15women/2018/games"
     link_ls = get_game_urls(event_slug)
     players = get_players(link_ls)
     df = pd.concat(players)
-    df.to_csv("u16americas_2021_players_game_stats.csv", index = None)
+    df.to_csv("u15oceania_2018_players_game_stats.csv", index = None)
