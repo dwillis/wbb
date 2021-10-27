84f8c4ca2683922893b86a82f5a5f160ae0bdbc6
Derek Willis <dwillis@gmail.com>
2021-10-27 08:04:33 -0400

    updated rosters and utils scrapers

--- ncaa/rosters.py
+++ ncaa/rosters.py
@@ -5,40 +5,122 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
+SEASONS = ['2021-22', '2020-21', '2019-20', '2018-19', '2017-18', '2016-17', '2015-16', '2014-15', '2013-14',
+'2012-13', '2011-12', '2010-11', '2009-10', '2008-09', '2007-08', '2006-07', '2005-06', '2004-05', '2003-04',
+'2002-03', '2001-02', '2000-01', '1999-00', '1998-99', '1997-98', '1996-97', '1995-96', '1994-95', '1993-94', '1992-93', '1991-92', '1990-91']
+
 def fetch_url(url):
     r = requests.get(url)
     return r
 
 def fetch_roster(base_url, season):
-    url = base_url + "roster/" + season
+    url = base_url + "/roster/" + season
     r = fetch_url(url)
+    if r.history:
+        return None
     return BeautifulSoup(r.text, features="html.parser")
 
-def parse_roster(html):
+def parse_script(soup):
+    scripts = soup.find_all('script')
+    script = [s for s in scripts if "roster:" in s.get_text()][0]
+    roster = []
+    match = re.search('roster: (.*),\r\n', script.get_text())
+    roster_json = json.loads(match.group(1))
+    for player in roster_json['players']:
+        roster.append(player)
+    return roster
+
+def get_teams():
+    results = []
+    teams = json.loads(open('teams.json').read())
+    teams_with_url = [t for t in teams if 'url' in t]
+    for team in teams_with_url:
+        print(team['ncaa_id'])
+        for season in SEASONS:
+            html = fetch_roster(team['url'], season)
+            if html:
+                filename = None
+                roster = parse_roster(html, season, filename, team)
+                if len(roster) > 0:
+                    print(f"adding {season}")
+                    results.append(roster)
+    with open("rosters.json", "w") as f:
+        json.dump(results, f)
+
+def get_team(team):
+    for season in SEASONS:
+        html = fetch_roster(team['url'], season)
+        filename = None
+#            filename = f"rosters/{team_id}_{season.replace('-','_')}.json"
+        roster = parse_roster(html, season, filename, team)
+    return roster
+
+def parse_roster(html, season, filename, team):
     roster = []
     players = html.find_all('li', {'class': 'sidearm-roster-player'})
     for player in players:
+        position = None
         if player.find('span', {'class': 'sidearm-roster-player-previous-school'}):
             previous_school = player.find('span', {'class': 'sidearm-roster-player-previous-school'}).text
         else:
             previous_school = None
         if player.find('span', {'class': 'sidearm-roster-player-highschool'}):
-            high_school = player.find('span', {'class': 'sidearm-roster-player-highschool'}).text
+            high_school_text = player.find('span', {'class': 'sidearm-roster-player-highschool'}).text.strip()
+            high_school = " ".join([x.strip() for x in high_school_text.split(' ') if x != ''])
         else:
             high_school = None
+        if player.find('span', {'class': 'sidearm-roster-player-height'}):
+            height = player.find('span', {'class': 'sidearm-roster-player-height'}).text
+        else:
+            height = None
+        try:
+            hometown = player.find('span', {'class': 'sidearm-roster-player-hometown'}).text
+        except:
+            hometown = None
+        if player.find('div', {'class': 'sidearm-roster-player-position'}).text.strip() == '':
+            position = 'N/A'
+        if not position and '"' in player.find('div', {'class': 'sidearm-roster-player-position'}).text.strip():
+            position = 'N/A'
+        if not position and player.find('div', {'class': 'sidearm-roster-player-position'}).find('span', {'class': 'text-bold'}).find('span', {'class': 'sidearm-roster-player-position-long-short hide-on-small-down'}):
+            try:
+                position = player.find('div', {'class': 'sidearm-roster-player-position'}).find('span', {'class': 'text-bold'}).find('span', {'class': 'sidearm-roster-player-position-long-short hide-on-small-down'}).text.strip()
+            except AttributeError:
+                position = None
+        if not position and player.find('div', {'class': 'sidearm-roster-player-position'}).find('span', {'class': 'text-bold'}):
+            try:
+                position = player.find('div', {'class': 'sidearm-roster-player-position'}).find('span', {'class': 'text-bold'}).text.strip()
+            except AttributeError:
+                position = None
+        try:
+            jersey = player.find('span', {'class': 'sidearm-roster-player-jersey-number'}).text.strip()
+        except:
+            jersey = None
+        try:
+            academic_year = player.find_all('span', {'class': 'sidearm-roster-player-academic-year'})[1].text
+        except:
+            academic_year = None
+        try:
+            name = player.find('a')['aria-label'].split(' - ')[0]
+        except:
+            name = player.find('h3').text.strip()
         roster.append({
-            'name': player.find('a')['aria-label'].split(' - ')[0],
-            'year': player.find_all('span', {'class': 'sidearm-roster-player-academic-year'})[1].text,
-            'hometown': player.find('span', {'class': 'sidearm-roster-player-hometown'}).text,
+            'team_id': team['ncaa_id'],
+            'team': team['team'],
+            'id': player['data-player-id'],
+            'name': name,
+            'year': academic_year,
+            'hometown': hometown,
             'high_school': high_school,
             'previous_school': previous_school,
-            'height': player.find('span', {'class': 'sidearm-roster-player-height'}).text,
-            'position': player.find('span', {'class': 'sidearm-roster-player-position-long-short'}).text.strip(),
-            'jersey': player.find('span', {'class': 'sidearm-roster-player-jersey-number'}).text.strip()
+            'height': height,
+            'position': position,
+            'jersey': jersey,
+            'url': player.find('a')['href'],
+            'season': season
         })
     return roster
 
-# Example usage: python rosters.py -season 2020-21 -url https://baylorbears.com/sports/womens-basketball/
+# Example usage: python rosters.py -season 2021-22 -url https://baylorbears.com/sports/womens-basketball/
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='NCAA team information')
@@ -46,5 +128,5 @@ if __name__ == "__main__":
     parser.add_argument('-url', action='store', dest='url', help='base url for a team')
     results = parser.parse_args()
     roster_html = fetch_roster(results.url, results.season)
-    roster = parse_roster(roster_html)
+    roster = parse_roster(roster_html, results.season)
     print(roster)
--- ncaa/utils.py
+++ ncaa/utils.py
@@ -19,6 +19,43 @@ def load_teams():
     teams_json = json.loads(open('teams.json').read())
     teams.insert_all(teams_json)
 
+def get_coaches_tweets(start):
+    db = Database('ncaa.db')
+    coaches_tweets_table = db['coaches_tweets']
+    coaches_json = json.loads(open('coaches.json').read())
+    for account in coaches_json[start:]:
+        tweets = []
+        print(account)
+        try:
+            user = api.get_user(account)
+#            r = db.execute(f"select min(id) from player_tweets where account_name = '{account}'")
+#            min_id = r.fetchone()
+#            for tweet in api.user_timeline(user.id, max_id=min_id[0]):
+            for tweet in api.user_timeline(user.id):
+                tw = {}
+                tw['account_name'] = account
+                tw['id'] = tweet.id
+                tw['url'] = f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id_str}"
+                tw['text'] = tweet.text
+                if 'retweeted_status' in tweet._json:
+                    tw['is_rt'] = True
+                    tw['rt_url'] = f"https://twitter.com/{tweet.retweeted_status.user.screen_name}/status/{tweet.retweeted_status.id}"
+                else:
+                    tw['is_rt'] = False
+                    tw['rt_url'] = None
+                tw['created_at'] = tweet.created_at
+                tw['in_reply_to'] = tweet.in_reply_to_status_id
+                tw['in_reply_to_name'] = tweet.in_reply_to_screen_name
+                tw['is_quote_status'] = tweet.is_quote_status
+                tw['retweet_count'] = tweet.retweet_count
+                tw['favorite_count'] = tweet.favorite_count
+                tweets.append(tw)
+        except tweepy.RateLimitError:
+            print('waiting...')
+            time.sleep(15 * 60)
+        except tweepy.error.TweepError:
+            continue
+        coaches_tweets_table.upsert_all(tweets, pk="id")
 
 def get_player_tweets(start):
     db = Database('ncaa.db')
@@ -51,20 +88,60 @@ def get_player_tweets(start):
                 tw['retweet_count'] = tweet.retweet_count
                 tw['favorite_count'] = tweet.favorite_count
                 tweets.append(tw)
-        except tweepy.error.TweepError:
-            continue
         except tweepy.RateLimitError:
             print('waiting...')
             time.sleep(15 * 60)
+        except tweepy.error.TweepError:
+            continue
         player_tweets_table.upsert_all(tweets, pk="id")
 
+def get_commit_tweets(start):
+    db = Database('ncaa.db')
+    commits_tweets_table = db['commit_tweets']
+    commits_json = json.loads(open('commits.json').read())
+    for account in commits_json[start:]:
+        tweets = []
+        print(account)
+        try:
+            user = api.get_user(account)
+#            r = db.execute(f"select min(id) from player_tweets where account_name = '{account}'")
+#            min_id = r.fetchone()
+#            for tweet in api.user_timeline(user.id, max_id=min_id[0]):
+            for tweet in api.user_timeline(user.id):
+                tw = {}
+                tw['account_name'] = account
+                tw['id'] = tweet.id
+                tw['url'] = f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id_str}"
+                tw['text'] = tweet.text
+                if 'retweeted_status' in tweet._json:
+                    tw['is_rt'] = True
+                    tw['rt_url'] = f"https://twitter.com/{tweet.retweeted_status.user.screen_name}/status/{tweet.retweeted_status.id}"
+                else:
+                    tw['is_rt'] = False
+                    tw['rt_url'] = None
+                tw['created_at'] = tweet.created_at
+                tw['in_reply_to'] = tweet.in_reply_to_status_id
+                tw['in_reply_to_name'] = tweet.in_reply_to_screen_name
+                tw['is_quote_status'] = tweet.is_quote_status
+                tw['retweet_count'] = tweet.retweet_count
+                tw['favorite_count'] = tweet.favorite_count
+                tweets.append(tw)
+        except tweepy.RateLimitError:
+            print('waiting...')
+            time.sleep(15 * 60)
+        except tweepy.error.TweepError:
+            continue
+        commits_tweets_table.upsert_all(tweets, pk="id")
+
 def get_team_tweets(start):
+    suspended = []
     db = Database('ncaa.db')
     tweets_table = db['tweets']
     teams_json = json.loads(open('teams.json').read())
     for team in teams_json[start:]:
         if 'private' in team:
-            continue
+            if team['private'] == True:
+                continue
         tweets = []
         print(team['twitter'])
         try:
@@ -86,10 +163,16 @@ def get_team_tweets(start):
                 tw['retweet_count'] = tweet.retweet_count
                 tw['favorite_count'] = tweet.favorite_count
                 tweets.append(tw)
-        except tweepy.RateLimitError:
+        except tweepy.error.TweepError as e:
+            if e.api_code == 63:
+                suspended.append(team['twitter'])
+            else:
+                continue
+        except tweepy.error.RateLimitError:
             print('waiting...')
             time.sleep(15 * 60)
         tweets_table.upsert_all(tweets, pk="id")
+    print(suspended)
 
 def update_following(start):
     db = Database('ncaa.db')
@@ -101,20 +184,70 @@ def update_following(start):
         following = []
         print(team['twitter'])
         try:
-            user = api.get_user(team['twitter'])
-            for friend in user.friends():
-                follow = {}
-                follow['team_id'] = team['ncaa_id']
-                follow['account_name'] = team['twitter']
-                follow['join_date'] = friend.created_at.strftime("%Y-%m-%d")
-                follow['id'] = friend.id
-                follow['username'] = friend.screen_name
-                follow['bio'] = friend.description
-                follow['name'] = friend.name
-                following.append(follow)
+            current_ids = api.friends_ids(team['twitter'])
+            r = db.execute(f"select id from following where account_name = '{team['twitter']}'")
+            ids = [i[0] for i in r.fetchall()]
+            ids_to_load = (list(list(set(current_ids)-set(ids)) + list(set(ids)-set(current_ids))))
+            groups = [ids_to_load[i * 100:(i + 1) * 100] for i in range((len(ids_to_load) + 100 - 1) // 100 )]
+            if len(groups) > 0:
+                for group in groups:
+                    try:
+                        users = api.lookup_users(group)
+                        for user in users:
+                            follow = {}
+                            follow['team_id'] = team['ncaa_id']
+                            follow['account_name'] = team['twitter']
+                            follow['join_date'] = user.created_at.strftime("%Y-%m-%d")
+                            follow['id'] = user.id
+                            follow['username'] = user.screen_name
+                            follow['bio'] = user.description
+                            follow['name'] = user.name
+                            follow['created_at'] = datetime.now().strftime("%Y-%m-%d")
+                            following.append(follow)
+                    except:
+                        continue
         except tweepy.RateLimitError:
             print('waiting...')
             time.sleep(15 * 60)
+        except tweepy.error.TweepError:
+            continue
+        following_table.upsert_all(following, pk=["id", "account_name"])
+
+def update_player_following(start):
+    db = Database('ncaa.db')
+    following_table = db['following']
+    players_json = json.loads(open('players.json').read())
+    for player in players_json[start:]:
+        following = []
+        print(player)
+        try:
+            current_ids = api.friends_ids(player)
+            r = db.execute(f"select id from following where account_name = '{player}'")
+            ids = [i[0] for i in r.fetchall()]
+            ids_to_load = (list(list(set(current_ids)-set(ids)) + list(set(ids)-set(current_ids))))
+            groups = [ids_to_load[i * 100:(i + 1) * 100] for i in range((len(ids_to_load) + 100 - 1) // 100 )]
+            if len(groups) > 0:
+                for group in groups:
+                    try:
+                        users = api.lookup_users(group)
+                        for user in users:
+                            follow = {}
+                            follow['team_id'] = None
+                            follow['account_name'] = player
+                            follow['join_date'] = user.created_at.strftime("%Y-%m-%d")
+                            follow['id'] = user.id
+                            follow['username'] = user.screen_name
+                            follow['bio'] = user.description
+                            follow['name'] = user.name
+                            follow['created_at'] = datetime.now().strftime("%Y-%m-%d")
+                            following.append(follow)
+                    except:
+                        continue
+        except tweepy.RateLimitError:
+            print('waiting...')
+            time.sleep(15 * 60)
+        except tweepy.error.TweepError:
+            continue
         following_table.upsert_all(following, pk=["id", "account_name"])
 
 def load_following_json(account, db):
