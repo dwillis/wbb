c2d4b6ec37e01d4fb1e5c15d289937fab6832ff8
Derek Willis <dwillis@gmail.com>
2021-11-28 16:09:40 -0500

    updated utils

--- ncaa/game_utils.py
+++ ncaa/game_utils.py
@@ -1,13 +1,13 @@
 import os
 import re
+import csv
 import json
 from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
 
-
 def fetch_rosters(id=None, seasons=None):
-    teams_json = json.loads(open('/Users/derekwillis/code/wbb/ncaa/teams.json').read())
+    teams_json = json.loads(open('/Users/dwillis/code/wbb/ncaa/teams.json').read())
     if not seasons:
         seasons = ['2020-21', '2019-20', '2018-19', '2017-18', '2016-17', '2015-16', '2014-15']
     if id:
@@ -28,7 +28,7 @@ def fetch_rosters(id=None, seasons=None):
                     continue
 
 def fetch_game_stats(id=None, seasons=None):
-    teams_json = json.loads(open('/Users/derekwillis/code/wbb/ncaa/teams.json').read())
+    teams_json = json.loads(open('/Users/dwillis/code/wbb/ncaa/teams.json').read())
     if not seasons:
         seasons = ['2021-22', '2020-21', '2019-20', '2018-19', '2017-18', '2016-17', '2015-16', '2014-15']
     if id:
@@ -70,12 +70,12 @@ def fetch_game_ids(season, stats_url):
     return game_ids
 
 def slugify(team):
-    slug = str(team['ncaa_id'])+'-'+team['team'].lower().replace(" ","-").replace('.','').replace(',','').replace("'","").replace(')','').replace(')','')
+    slug = str(team['ncaa_id'])+'-'+team['team'].lower().replace(" ","-").replace('.','').replace(',','').replace("'","").replace(')','').replace('(','')
     return slug
 
 def parse_roster(season, slug):
     results = []
-    os.chdir("/Users/derekwillis/code/wbb-rosters")
+    os.chdir("/Users/dwillis/code/wbb-rosters")
     if not os.path.exists(slug):
         os.makedirs(slug)
     os.chdir(slug)
@@ -88,7 +88,7 @@ def parse_roster(season, slug):
 
 def parse_games(season, domain, game_ids, slug):
     results = []
-    os.chdir("/Users/derekwillis/code/wbb-game-data")
+    os.chdir("/Users/dwillis/code/wbb-game-data")
     if not os.path.exists(slug):
         os.makedirs(slug)
     os.chdir(slug)
@@ -121,3 +121,53 @@ def write_json(game_id, game_json, season):
     filename = str(game_id) + '.json'
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(game_json, f, ensure_ascii=False, indent=4)
+
+def parse_game_json(slug, season, game_id):
+    os.chdir("/Users/dwillis/code/wbb-game-data")
+    os.chdir(slug)
+    os.chdir(season)
+    return json.loads(open(game_id+'.json').read())
+
+def parse_turnovers(team, slug, season, game_id):
+    game_json = parse_game_json(slug, season, game_id)
+    if game_json['Plays']:
+        for play in game_json['Plays']:
+            if play['Type'] == 'TURNOVER':
+                if play['Player'] == None:
+                    t = play['Team']
+                    team = game_json['Game'][t]['Name']
+                    uniform = None
+                else:
+                    t = play['Player']['Team']
+                    team = game_json['Game'][t]['Name']
+                    uniform = play['Player']['UniformNumber']
+                if t == 'VistingTeam':
+                    opp = 'HomeTeam'
+                else:
+                    opp = 'VisitingTeam'
+                opponent = game_json['Game'][opp]['Name']
+    return [id, game_id, game_json['Game']['Date'], team, opponent, play['Period'], play['ClockSeconds'], uniform]
+
+def get_all_turnovers(season):
+    turnovers = []
+    missing = []
+    teams_json = json.loads(open('/Users/dwillis/code/wbb/ncaa/teams.json').read())
+    for team in teams_json:
+        slug = slugify(team)
+        try:
+            os.chdir(f"/Users/dwillis/code/wbb-game-data/{slug}/{season}")
+        except:
+            missing.append(team['ncaa_id'])
+            continue
+        print(team['ncaa_id'])
+        for root, dirs, files in os.walk(".", topdown=False):
+            for file in files:
+                if file == '.DS_Store':
+                    continue
+                game_id = file.split('.')[0]
+                print(game_id)
+                turnovers.append(parse_turnovers(team, slug, season, game_id))
+    with open(f"turnovers_{season}.csv", 'w') as output_file:
+        csv_file = csv.writer(output_file)
+        csv_file.writerow(['id', 'game_id', 'date', 'team', 'opponent', 'period', 'seconds', 'player'])
+        csv_file.writerows(turnovers)
