55b14ca4157ce70c6c6545874c8fc7069cafcb2d
Derek Willis <dwillis@gmail.com>
2023-12-26 08:56:54 -0500

    fixed Notre Dame url and more pbp

--- ncaa/other_pbp.py
+++ ncaa/other_pbp.py
@@ -7,14 +7,16 @@ def slugify(team):
     slug = str(team['ncaa_id'])+'-'+team['team'].lower().replace(" ","-").replace('.','').replace(',','').replace("'","").replace(')','').replace('(','')
     return slug
 
-def pbp_for_season(season="2023-24"):
+def pbp_for_season(season="2023-24", team_ids=[312, 334, 365, 513, 328, 473, 736, 519, 746, 415, 648]):
     with open("teams.json", "r") as file:
         teams = json.load(file)
-    team_ids = [312, 334, 365, 513, 328, 473, 736, 519, 746, 415, 648]
     for team_id in team_ids:
         team = [t for t in teams if t['ncaa_id'] == team_id][0]
         print(team['team'])
-        boxscore_links = boxscore_links_for_season(team, season)
+        if team_id in [513, 365]:
+            boxscore_links = boxscore_links_for_season_direct(team, season)
+        else:
+            boxscore_links = boxscore_links_for_season(team, season)
         for url in boxscore_links:
             id = parse_boxscore_for_id(url)
             get_plays(id, team, season)
@@ -26,8 +28,24 @@ def boxscore_links_for_season(team, season):
     boxscore_links = [team['url'].split('/sports/')[0] + l['href'] for l in soup.find_all('a', class_='schedule-event-link--boxscore')]
     return boxscore_links
 
+def boxscore_links_for_season_direct(team, season):
+    url = f"{team['url']}/schedule/season/{season}/"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    }
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+    if team['ncaa_id'] == 513:
+        boxscore_links = [l['href'] for l in soup.find_all('a') if '/boxscore/' in l['href']]
+    else:
+        boxscore_links = [team['url'].split('/sports/')[0] + l['href'] for l in soup.find_all('a') if '/boxscore/' in l['href']]
+    return boxscore_links
+
 def parse_boxscore_for_id(url):
-    r = requests.get(url)
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    }
+    r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
     id = soup.find("wmt-stats-iframe")['path'].split('/')[-1]
     return id
@@ -48,4 +66,4 @@ def get_plays(id, team, season):
         os.chdir(season)
         json_file_path = f'{id}.json'
         with open(json_file_path, 'w') as json_file:
-            json.dump(game['data']['plays']['data'], json_file, indent=4)
+            json.dump(game, json_file, indent=4)
